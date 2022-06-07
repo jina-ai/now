2997bac3fcf1da582e53e3e42ed0442d564869f3
Mohammad Kalim Akram <kalim.akram@jina.ai>
2022-06-07 13:02:06 +0200

    fix: fix broken test

--- tests/unit/bff/test_app.py
+++ tests/unit/bff/test_app.py
@@ -1,25 +1,23 @@
 import pytest
 
-from now.constants import Apps
-
 
 @pytest.mark.parametrize(
-    'app', [Apps.TEXT_TO_IMAGE, Apps.IMAGE_TO_TEXT, Apps.IMAGE_TO_IMAGE]
+    'inp_mod, output_mod', [('text', 'image'), ('image', 'text'), ('image', 'image')]
 )
 class TestParametrized:
-    def test_check_liveness(self, test_client, app):
-        response = test_client.get(f'/api/v1/{app}/ping')
+    def test_check_liveness(self, test_client, inp_mod, output_mod):
+        response = test_client.get(f'/api/v1/{inp_mod}-to-{output_mod}/ping')
         assert response.status_code == 200
         assert response.json() == 'pong!'
 
-    def test_read_root(self, test_client, app):
-        response = test_client.get(f'/api/v1/{app}')
+    def test_read_root(self, test_client, inp_mod, output_mod):
+        response = test_client.get(f'/api/v1/{inp_mod}-to-{output_mod}')
         assert response.status_code == 200
 
-    def test_get_docs(self, test_client, app):
-        response = test_client.get(f'/api/v1/{app}/docs')
+    def test_get_docs(self, test_client, inp_mod, output_mod):
+        response = test_client.get(f'/api/v1/{inp_mod}-to-{output_mod}/docs')
         assert response.status_code == 200
 
-    def test_get_redoc(self, test_client, app):
-        response = test_client.get(f'/api/v1/{app}/redoc')
+    def test_get_redoc(self, test_client, inp_mod, output_mod):
+        response = test_client.get(f'/api/v1/{inp_mod}-to-{output_mod}/redoc')
         assert response.status_code == 200
