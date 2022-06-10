854c14aa49a734a32f60645376b3e60e4c8fbab1
Joschka Braun <joschka.braun@gmail.com>
2022-06-10 09:44:51 +0200

    feat(text-to-text): introduce smaller text model for tests

--- now/apps/text_to_text/app.py
+++ now/apps/text_to_text/app.py
@@ -39,13 +39,15 @@ class TextToText(JinaNOWApp):
 
     @property
     def pre_trained_embedding_size(self) -> Dict[Qualities, int]:
-        return {
-            Qualities.MEDIUM: 384,
-        }
+        return {Qualities.MEDIUM: 384, Qualities.GOOD: 384}
 
     def setup(
         self, da: DocumentArray, user_config: UserInput, kubectl_path: str
     ) -> Dict:
+        quality_pretrained_model_map = {
+            Qualities.MEDIUM: 'paraphrase-MiniLM-L3-v2',
+            Qualities.GOOD: 'sentence-transformers/all-MiniLM-L6-v2',
+        }
         return finetune_flow_setup(
             self,
             da,
@@ -53,7 +55,9 @@ class TextToText(JinaNOWApp):
             kubectl_path,
             encoder_uses='TransformerTorchEncoder/v0.4',
             encoder_uses_with={
-                'pretrained_model_name_or_path': 'sentence-transformers/all-MiniLM-L6-v2'
+                'pretrained_model_name_or_path': quality_pretrained_model_map[
+                    user_config.quality
+                ]
             },
             indexer_uses='DocarrayIndexer',
         )
--- now/constants.py
+++ now/constants.py
@@ -1,6 +1,6 @@
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.57-text2text-5'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.57-text2text-6'
 
 
 class Modalities(BetterEnum):
